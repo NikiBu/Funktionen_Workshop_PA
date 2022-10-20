bcaf1c93e578675cda436dc29b3ec9e7e466534b
NikiBu <102289799+NikiBu@users.noreply.github.com>
2022-10-20 12:14:02 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,41 +1,3 @@
-#forward & backward time in sekunden angeben wie lange fahren,
-#speed wie schnell 1 = vollgas 0.5 = halbgas
-#abbiegen: winkel in grad angeben 0 - 360
-
-def forward(time: number, speed: number):
-    pins.analog_write_pin(AnalogPin.P15, 1023 * speed)
-    pins.analog_write_pin(AnalogPin.P14, 1023 * speed)
-    basic.pause(time * 1000)
-    pins.analog_write_pin(AnalogPin.P15, 0)
-    pins.analog_write_pin(AnalogPin.P14, 0)
-
-def backward(time: number, speed: number):
-    pins.analog_write_pin(AnalogPin.P13, 1023 * speed)
-    pins.analog_write_pin(AnalogPin.P12, 1023 * speed)
-    basic.pause(time * 1000)
-    pins.analog_write_pin(AnalogPin.P13, 0)
-    pins.analog_write_pin(AnalogPin.P12, 0)
-
-def rechts_abbiegen(winkel: number):
-    pins.analog_write_pin(AnalogPin.P15, 1023)
-    pins.analog_write_pin(AnalogPin.P12, 1023)
-    basic.pause(winkel)
-    pins.analog_write_pin(AnalogPin.P15, 0)
-    pins.analog_write_pin(AnalogPin.P12, 0)
-
-def links_abbiegen(winkel: number):
-
-    pins.analog_write_pin(AnalogPin.P13, 1023)
-    pins.analog_write_pin(AnalogPin.P14, 1023)
-    basic.pause(winkel)
-    pins.analog_write_pin(AnalogPin.P13, 0)
-    pins.analog_write_pin(AnalogPin.P14, 0)
-
-time = 0
-winkel = 0
-speed = 0
-x=0
-
 # m1a output auf P15
 # m1b auf p13
 # m2a auf p14
@@ -46,13 +8,44 @@ x=0
 # 3.3v pin
 
 def on_button_pressed_a():
-    global x
-
     basic.show_number(x)
     rechts_abbiegen(x)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def backward(time: number, speed: number):
+    pins.analog_write_pin(AnalogPin.P13, 1023 * speed)
+    pins.analog_write_pin(AnalogPin.P12, 1023 * speed)
+    basic.pause(time * 1000)
+    pins.analog_write_pin(AnalogPin.P13, 0)
+    pins.analog_write_pin(AnalogPin.P12, 0)
+def rechts_abbiegen(winkel: number):
+    pins.analog_write_pin(AnalogPin.P15, 1023)
+    pins.analog_write_pin(AnalogPin.P12, 1023)
+    basic.pause(winkel)
+    pins.analog_write_pin(AnalogPin.P15, 0)
+    pins.analog_write_pin(AnalogPin.P12, 0)
+
 def on_button_pressed_b():
     global x
-    x = x+100
+    x = x + 100
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# forward & backward time in sekunden angeben wie lange fahren,
+# speed wie schnell 1 = vollgas 0.5 = halbgas
+# abbiegen: winkel in grad angeben 0 - 360
+def forward(time: number, speed: number):
+    pins.analog_write_pin(AnalogPin.P15, 1023 * speed)
+    pins.analog_write_pin(AnalogPin.P14, 1023 * speed)
+    basic.pause(time * 1000)
+    pins.analog_write_pin(AnalogPin.P15, 0)
+    pins.analog_write_pin(AnalogPin.P14, 0)
+def links_abbiegen(winkel: number):
+    pins.analog_write_pin(AnalogPin.P13, 1023)
+    pins.analog_write_pin(AnalogPin.P14, 1023)
+    basic.pause(winkel)
+    pins.analog_write_pin(AnalogPin.P13, 0)
+    pins.analog_write_pin(AnalogPin.P14, 0)
+x = 0
+speed = 0
+winkel = 0
+time = 0
--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
-// forward & backward time in sekunden angeben wie lange fahren,
-// speed wie schnell 1 = vollgas 0.5 = halbgas
-// abbiegen: winkel in grad angeben 0 - 360
-function forward(time: number, speed: number) {
-    pins.analogWritePin(AnalogPin.P15, 1023 * speed)
-    pins.analogWritePin(AnalogPin.P14, 1023 * speed)
-    basic.pause(time * 1000)
-    pins.analogWritePin(AnalogPin.P15, 0)
-    pins.analogWritePin(AnalogPin.P14, 0)
-}
-
+//  m1a output auf P15
+//  m1b auf p13
+//  m2a auf p14
+//  m2b auf p12
+//  input ins grüne +/- (5V)
+//  trigger = P8
+//  echo = P9
+//  3.3v pin
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showNumber(x)
+    rechts_abbiegen(x)
+})
 function backward(time: number, speed: number) {
     pins.analogWritePin(AnalogPin.P13, 1023 * speed)
     pins.analogWritePin(AnalogPin.P12, 1023 * speed)
@@ -25,6 +26,25 @@ function rechts_abbiegen(winkel: number) {
     pins.analogWritePin(AnalogPin.P12, 0)
 }
 
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    x = x + 100
+})
+//  forward & backward time in sekunden angeben wie lange fahren,
+//  speed wie schnell 1 = vollgas 0.5 = halbgas
+//  abbiegen: winkel in grad angeben 0 - 360
+function forward(time: number, speed: number) {
+    pins.analogWritePin(AnalogPin.P15, 1023 * speed)
+    pins.analogWritePin(AnalogPin.P14, 1023 * speed)
+    basic.pause(time * 1000)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P14, 0)
+}
+
+/**
+  * Lässt den Roboter um den gegebenen Winkel links abbiegen
+  */
+    //% block
 function links_abbiegen(winkel: number) {
     pins.analogWritePin(AnalogPin.P13, 1023)
     pins.analogWritePin(AnalogPin.P14, 1023)
@@ -33,24 +53,7 @@ function links_abbiegen(winkel: number) {
     pins.analogWritePin(AnalogPin.P14, 0)
 }
 
-let time = 0
-let winkel = 0
-let speed = 0
 let x = 0
-//  m1a output auf P15
-//  m1b auf p13
-//  m2a auf p14
-//  m2b auf p12
-//  input ins grüne +/- (5V)
-//  trigger = P8
-//  echo = P9
-//  3.3v pin
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    basic.showNumber(x)
-    rechts_abbiegen(x)
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    x = x + 100
-})
+let speed = 0
+let winkel = 0
+let time = 0
